87a6bfa229f04f18550eedf1a60c73be62a74b27
zhangshaoxuan <web_zsx@163.com>
2019-05-09 17:30:52 +0800

    update the path of background images in less

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,69 @@
-const { injectBabelPlugin } = require('react-app-rewired');
+/*const { injectBabelPlugin } = require('react-app-rewired');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = function override(config, env) {
-    console.log(config);
+    if(env === "production"){
+        // config.output.publicPath = '/react-app-github/';
+        config.module.rules[2].oneOf.splice(0,1,{
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            use: [{
+                loader: require.resolve('url-loader'),
+                options: {
+                  limit: 10000,
+                  name: '../images/[name].[hash:8].[ext]',
+                  publicPath: '/react-app-github/'
+                  // outputPath: 'images/',
+                  // publicPath : '/images'
+                }
+            }]
+        });
+        // config.module.rules[2].oneOf.push({
+        //         test: /\.css$/,
+        //         use: [{
+        //             loader: MiniCssExtractPlugin.loader,
+        //             options: { publicPath: '../../' }
+        //         },{
+        //             loader: require.resolve('css-loader')
+        //         }]
+        //     });
+    }
+    config.module.rules[2].oneOf.forEach(element => {
+        console.log(element); 
+    });
+    // console.log(config.module.rules[2].oneOf[3].use);
     config = injectBabelPlugin(['import', { libraryName: 'antd-mobile', style: 'css' }], config);
+
     return config;
 };
+*/
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { override, fixBabelImports, addWebpackAlias } = require('customize-cra');
+const path = require("path");
+
+const rewiredMap = () => config => {
+  if(config.mode === 'production'){
+    console.log(require.resolve('url-loader'));
+    config.module.rules[2].oneOf.splice(0,1,{
+      test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+      use: [{
+          loader: require.resolve('url-loader'),
+          options: {
+            limit: 10000,
+            name: 'images/[name].[hash:8].[ext]'
+          }
+      }]
+    });
+  }
+  config.resolve.extensions.push('.less');
+  return config;
+};
+
+module.exports = override(
+  rewiredMap(),
+  fixBabelImports('import', {
+    libraryName: 'antd-mobile',
+    style: 'css',
+  }),
+  addWebpackAlias({
+    ["aliasimgurl"]: path.resolve(__dirname, "src/assets/images")
+  }),
+);
